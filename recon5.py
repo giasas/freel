3a1713f364ab3085beef3d32345dcf4c83b12685
giasas <giasas@users.noreply.github.com>
2023-07-28 11:25:07 +0300

    Update recon6.py

--- recon5.py
+++ recon5.py
@@ -1,14 +1,18 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 def calculate_unmatched(df1, df2, col_mappings):
     # Adjust columns based on user input
-    df1 = df1[[col_mappings['Date1'], col_mappings['Description1'], col_mappings['Debit1'], col_mappings['Credit1'], col_mappings['TransNo1'], col_mappings['RefNo1']]]
-    df2 = df2[[col_mappings['Date2'], col_mappings['Description2'], col_mappings['Debit2'], col_mappings['Credit2']]]
+    df1_cols = [col_mappings[key] for key in ['Date1', 'Description1', 'Debit1', 'Credit1'] if col_mappings[key] is not None]
+    df1 = df1[df1_cols]
+    
+    df2_cols = [col_mappings[key] for key in ['Date2', 'Description2', 'Debit2', 'Credit2'] if col_mappings[key] is not None]
+    df2 = df2[df2_cols]
 
     # Rename columns for consistency
-    df1.columns = ['Date', 'Description', 'Debit', 'Credit', 'Trans. No.', 'Ref. No.']
-    df2.columns = ['Date', 'Description', 'Debit', 'Credit']
+    df1.columns = ['Date', 'Description', 'Debit', 'Credit'][:len(df1_cols)]
+    df2.columns = ['Date', 'Description', 'Debit', 'Credit'][:len(df2_cols)]
 
     # Merge dataframes and identify unmatched rows
     df_all = pd.concat([df1.assign(Source='File 1'), df2.assign(Source='File 2')])
@@ -36,16 +40,16 @@ def upload_form():
             df2 = pd.read_excel(file2)
 
         col_mappings = {
-            'Date1': st.selectbox('Select the Date column from first file', df1.columns, index=0),
-            'Description1': st.selectbox('Select the Description column from first file', df1.columns, index=1),
-            'Debit1': st.selectbox('Select the Debit column from first file', df1.columns),
-            'Credit1': st.selectbox('Select the Credit column from first file', df1.columns),
-            'TransNo1': st.selectbox('Select the Trans. No. column from first file', df1.columns),
-            'RefNo1': st.selectbox('Select the Ref. No. column from first file', df1.columns),
-            'Date2': st.selectbox('Select the Date column from bank statement', df2.columns, index=0),
-            'Description2': st.selectbox('Select the Description column from bank statement', df2.columns, index=1),
-            'Debit2': st.selectbox('Select the Debit column from bank statement', df2.columns),
-            'Credit2': st.selectbox('Select the Credit column from bank statement', df2.columns)
+            'Date1': st.selectbox('Select the Date column from first file', [None] + list(df1.columns), index=1),
+            'Description1': st.selectbox('Select the Description column from first file', [None] + list(df1.columns), index=2),
+            'Debit1': st.selectbox('Select the Debit column from first file', [None] + list(df1.columns)),
+            'Credit1': st.selectbox('Select the Credit column from first file', [None] + list(df1.columns)),
+            'TransNo1': st.selectbox('Select the Trans. No. column from first file (optional)', [None] + list(df1.columns)),
+            'RefNo1': st.selectbox('Select the Ref. No. column from first file (optional)', [None] + list(df1.columns)),
+            'Date2': st.selectbox('Select the Date column from bank statement', [None] + list(df2.columns), index=1),
+            'Description2': st.selectbox('Select the Description column from bank statement', [None] + list(df2.columns), index=2),
+            'Debit2': st.selectbox('Select the Debit column from bank statement', [None] + list(df2.columns)),
+            'Credit2': st.selectbox('Select the Credit column from bank statement', [None] + list(df2.columns))
         }
 
         df_unmatched = calculate_unmatched(df1, df2, col_mappings)
@@ -73,3 +77,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
